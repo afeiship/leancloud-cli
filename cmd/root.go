3b8ba8e75a69b2065dbdbd314859c48167538fc5
afeiship <1290657123@qq.com>
2022-11-20 12:51:20 +0800

    feat: basic get value api

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"lcc/misc"
 	"os"
 )
 
@@ -11,7 +13,17 @@ var rootCmd = &cobra.Command{
 	Short: "Leancloud cli for options curd.",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, world!")
+		// get optionKey
+		optionKey, _ := cmd.Flags().GetString("optionKey")
+		// Get value by optionKey
+		if optionKey != "" {
+			fmt.Println("optionKey:", optionKey)
+			res := misc.LcGet(optionKey)
+			fmt.Println("res:", res)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -32,5 +44,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// get value by optionKey
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("optionKey", "k", "", "Get value by option key")
 }
--- misc/index.go
+++ misc/index.go
@@ -1,10 +1,76 @@
 package misc
 
-import "github.com/go-resty/resty/v2"
+import (
+	"encoding/json"
+	"github.com/go-resty/resty/v2"
+	"log"
+	"os"
+)
 
-func GetLc(key) {
+type LcResponseBody struct {
+	Key       string `json:"key"`
+	Value     string `json:"value"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+	ObjectId  string `json:"objectId"`
+}
+
+func GetUrl(key string) string {
+	return "https://8alqie7r.lc-cn-n1-shared.com/1.1/classes/options/" + key
+}
+
+func LcGet(optionKey string) LcResponseBody {
+	url := GetUrl(optionKey)
 	client := resty.New()
-	// format url
-	// url := "https://8alqie7r.lc-cn-n1-shared.com/1.1/classes/options/" + key
-	
+	// set header Lc_key, lc_id from os.get env
+	lcId := os.Getenv("LC_ID")
+	lcKey := os.Getenv("LC_KEY")
+	client.SetHeaders(map[string]string{"X-LC-Id": lcId, "X-LC-Key": lcKey})
+	resp, err := client.R().Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var lcResponseBody LcResponseBody
+	json.Unmarshal(resp.Body(), &lcResponseBody)
+	return lcResponseBody
+}
+
+// create item by key/value
+func LcPost(optionKey string, key, value string) LcResponseBody {
+	url := GetUrl(optionKey)
+	client := resty.New()
+	resp, err := client.R().SetBody(map[string]string{
+		"key":   key,
+		"value": value,
+	}).Post(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var lcResponseBody LcResponseBody
+	json.Unmarshal(resp.Body(), &lcResponseBody)
+	return lcResponseBody
+}
+
+func LcPut(optionKey string, key, value string) LcResponseBody {
+	url := GetUrl(optionKey)
+	client := resty.New()
+	resp, err := client.R().SetBody(map[string]string{
+		"key":   key,
+		"value": value,
+	}).Put(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var lcResponseBody LcResponseBody
+	json.Unmarshal(resp.Body(), &lcResponseBody)
+	return lcResponseBody
+}
+
+func LcDelete(optionKey string) {
+	url := GetUrl(optionKey)
+	client := resty.New()
+	_, err := client.R().Delete(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
